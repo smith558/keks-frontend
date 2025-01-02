dd5e9bf98e291498e0f638cf3d79baa8c74eebb0
Stanislav Modrak <stanislav.modrak@gmail.com>
2025-01-02 17:33:38 +0100

    Add button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-    return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <Button>Button</Button>
+    </div>
+  );
 }
